2c9e64ce0d241abf6401faf2e63c595bf09b4dcb
GabdukaevKonevskih1516 <konevskihruslan@gmail.com>
2026-03-29 14:58:39 +0500

    main.cpp

--- задание_3/задание_3_версия_АлисаAI/main.cpp
+++ задание_3/задание_3_версия_АлисаAI/main.cpp
@@ -1,1 +1,22 @@
+#include "Header.h"
+#include <iostream>
 
+    int main() {
+
+        // Установка кодировки для корректного отображения кириллицы (для Windows)
+        setlocale(LC_ALL, "Russian");
+
+        TGraphProcessor processor;
+
+        // Замените "FileName" на имя вашего файла с матрицей смежности
+        if (processor.LoadFromFile("FileName.txt")) {
+            std::vector<int> degrees = processor.CalculateVertexDegrees();
+            processor.PrintDegrees(degrees);
+        }
+        else {
+            std::cout << "Не удалось обработать граф" << std::endl;
+            return 1;
+        }
+
+        return 0;
+    }
