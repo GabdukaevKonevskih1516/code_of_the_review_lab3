9591fb8f7689b40ba8e97d23b65b4eef99ca1cc3
GabdukaevKonevskih1516 <konevskihruslan@gmail.com>
2026-03-29 15:01:46 +0500

    main.cpp

--- задание_3/задание_3_версия_джимини/main.cpp
+++ задание_3/задание_3_версия_джимини/main.cpp
@@ -1,1 +1,29 @@
+// Copyright (c) 2026. All Rights Reserved.
 
+#include "Header.h"
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	std::string FileName;
+	std::cout << "Введите имя файла с графом (например, graph_data.txt): ";
+	std::cin >> FileName;
+
+	TGraphProcessor Processor;
+
+	if (Processor.LoadFromFile(FileName))
+	{
+		std::vector<int32> Degrees = Processor.CalculateVertexDegrees();
+		std::cout << "-----------------------------------\n";
+		Processor.PrintDegrees(Degrees);
+		std::cout << "-----------------------------------\n";
+	}
+	else
+	{
+		std::cerr << "Ошибка: Не удалось прочитать файл или формат данных неверен.\n";
+	}
+
+	return 0;
+}
