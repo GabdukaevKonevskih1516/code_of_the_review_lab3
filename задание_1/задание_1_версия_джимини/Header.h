11d4c08026db3babfa5b2421fd7ab22f1fdd7d8f
GabdukaevKonevskih1516 <konevskihruslan@gmail.com>
2026-03-29 14:52:12 +0500

    Header.h

--- задание_1/задание_1_версия_джимини/Header.h
+++ задание_1/задание_1_версия_джимини/Header.h
@@ -1,1 +1,29 @@
+// Copyright (c) 2026. All Rights Reserved.
 
+#pragma once
+
+#include <string>
+#include <cstdint> // Добавляем этот заголовок
+
+// Определяем типы в стиле Epic Games для обычного компилятора
+typedef std::uint8_t uint8;
+typedef std::int32_t int32;
+
+/** Уровни сложности пароля */
+enum class EPasswordDifficulty : uint8
+{
+	Low,
+	Medium,
+	High
+};
+/**
+ * Генерирует случайный пароль на основе выбранного уровня сложности
+ * @param InDifficulty - Выбранная сложность
+ * @return Сгенерированная строка пароля
+ */
+std::string GeneratePassword(EPasswordDifficulty InDifficulty);
+
+/**
+ * Очищает поток ввода и обрабатывает ошибки ввода пользователя
+ */
+void ClearInputBuffer();
